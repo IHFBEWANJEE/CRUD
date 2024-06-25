4e383faeb271d26fc1658854c10979196159262c
IHFBEWANJEE <jeeeh@ihateflyingbugs.com>
2024-06-25 16:25:13 +0900

    add / CRUD

--- src/member/controller/member.controller.ts
+++ src/member/controller/member.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { MemberService } from "../service/member.service";
+import { MemberRequest } from "../service/dto/request/member.dto.request";
 
 @Controller("member")
-export class MemberController { }
+export class MemberController {
+    constructor(private memberService: MemberService) { }
+    @Get("all")
+    findAllMembers(){
+        return this.memberService.getAllMembers()
+    }
+    @Post("register")
+    registerMember(@Body() memberRequest: MemberRequest){
+        return this.memberService.registerMember(memberRequest)
+    }
+    @Put("update/:id")
+    updateMember(@Param("id") id: number, @Body('name') newName: string) {
+        return this.memberService.updateMemberName(id, newName)
+    }
+    @Delete("delete/:id")
+    deleteMember(@Param("id") id: number){
+        return this.memberService.deleteById(id)
+    }
+}
