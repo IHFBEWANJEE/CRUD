6f19c84158c0ca68b3374f8384f7a326fbe6eb6e
IHFBEWANJEE <jeeeh@ihateflyingbugs.com>
2024-06-25 15:28:05 +0900

    add / registerMember

--- src/member/service/member.service.ts
+++ src/member/service/member.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Member } from "../entity/member.entity";
+import { Repository } from "typeorm";
+import { MemberRequest } from "./dto/request/member.dto.request";
 
 @Injectable()
-export class MemberService { }
+export class MemberService {
+    constructor(
+        @InjectRepository(Member)
+        private memberRepository: Repository<Member>) { }
+
+    async registerMember(memberRequest: MemberRequest): Promise<Member>{
+        const member = new Member()
+        member.member_id = memberRequest.member_id
+        member.name = memberRequest.name
+        member.password = memberRequest.password
+        await this.memberRepository.save(member)
+        return member
+    }
+ }
