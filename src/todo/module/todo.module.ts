d1efe92cd1bc978d542e261b19b97b66112565b0
IHFBEWANJEE <jeeeh@ihateflyingbugs.com>
2024-06-25 14:55:42 +0900

    add : Todo 모듈 생성

---
+++ src/todo/module/todo.module.ts
@@ -0,0 +1,14 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Todo } from "../entity/todo.entity";
+import { TodoController } from "../controller/todo.controller";
+import { TodoService } from "../service/todo.service";
+
+@Module({
+    imports: [
+        TypeOrmModule.forFeature([Todo])
+    ],
+    controllers: [TodoController],
+    providers: [TodoService]
+})
+export class TodoModule { }
