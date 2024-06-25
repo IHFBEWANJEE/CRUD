d50eb61c800dd9bc161cc30b5a75738bef9119c1
IHFBEWANJEE <jeeeh@ihateflyingbugs.com>
2024-06-25 18:28:40 +0900

    add / repository & 생성자

--- src/todo/service/todo.service.ts
+++ src/todo/service/todo.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from "@nestjs/common";
+import {Injectable} from "@nestjs/common";
+import {Todo} from "../entity/todo.entity";
+import {InjectRepository} from "@nestjs/typeorm";
+import {Repository} from "typeorm";
+import {TodoRequest} from "./dto/request/todo.dto.request";
 
 @Injectable()
-export class TodoService { }
+export class TodoService {
+    constructor(
+        @InjectRepository(Todo)
+        private todoRepository: Repository<Todo>) {
+    }
+
+    async createTodo(todoRequest: TodoRequest): Promise<Todo> {
+        const todo = new Todo(todoRequest);
+        return await this.todoRepository.save(todo)
+    }
+}
