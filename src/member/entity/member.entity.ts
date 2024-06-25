412e7f0298b65e8d260bce4ca56a6f35933c588a
IHFBEWANJEE <jeeeh@ihateflyingbugs.com>
2024-06-25 18:07:11 +0900

    edit : 연관관계 매핑 변경

--- src/member/entity/member.entity.ts
+++ src/member/entity/member.entity.ts
@@ -24,6 +24,6 @@ export class Member{
     @Column()
     name: string
 
-    @OneToMany(() => Todo, (todo) => todo.id)
+    @OneToMany(() => Todo, (todo) => todo.member)
     todos: Todo[]
 }
--- src/todo/entity/todo.entity.ts
+++ src/todo/entity/todo.entity.ts
@@ -1,15 +1,24 @@
-import { CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { Member } from "../../member/entity/member.entity";
 
 @Entity("todo")
 export class Todo{
     @PrimaryGeneratedColumn()
     id: number
+
+    @Column()
+    title: string
+
+    @Column()
+    contents: string
+
     @CreateDateColumn()
     createdAt: Date
+
     @UpdateDateColumn()
     updatedAt: Date
-    @ManyToOne(() => Member, (member) => member.id)
+
+    @ManyToOne(() => Member, (member) => member.todos)
     @JoinColumn({ name: 'member_id' })
     member: Member
 }
