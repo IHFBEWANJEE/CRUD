f85279f0e099ab2da3123d8e0c0410e82fb8f743
IHFBEWANJEE <jeeeh@ihateflyingbugs.com>
2024-06-26 10:55:29 +0900

    add / 메서드 추가

--- src/todo/service/todo.service.ts
+++ src/todo/service/todo.service.ts
@@ -3,16 +3,27 @@ import {Todo} from "../entity/todo.entity";
 import {InjectRepository} from "@nestjs/typeorm";
 import {Repository} from "typeorm";
 import {TodoRequest} from "./dto/request/todo.dto.request";
+import {Member} from "../../member/entity/member.entity";
 
 @Injectable()
 export class TodoService {
     constructor(
         @InjectRepository(Todo)
-        private todoRepository: Repository<Todo>) {
+        private todoRepository: Repository<Todo>,
+        @InjectRepository(Member)
+        private memberRepository: Repository<Member>) {
     }
 
-    async createTodo(todoRequest: TodoRequest): Promise<Todo> {
-        const todo = new Todo(todoRequest);
-        return await this.todoRepository.save(todo)
+    async createTodo(todoRequest: TodoRequest, memberId: number): Promise<Todo> {
+        const todo: Todo = new Todo(todoRequest);
+        const member: Member = await this.memberRepository.findOneBy({id: memberId});
+        await member.addTodo(todo);
+        return await this.todoRepository.save(todo);
+    }
+
+    async deleteTodo(todoId: number, memberId: number): Promise<string> {
+        const todo: Todo = await this.todoRepository.findOneBy({id: todoId});
+        const member: Member = await this.memberRepository.findOneBy({id: memberId});
+        return member.deleteTodo(todo) ? `successfully deleted todo with id : ${todoId} ` : "something was wrong";
     }
 }
