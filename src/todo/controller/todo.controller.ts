07cd12fad0ffd88b0e1fb7f88c9aed4a0cd2fc6c
IHFBEWANJEE <jeeeh@ihateflyingbugs.com>
2024-06-26 10:54:41 +0900

    add / 메서드

--- src/todo/controller/todo.controller.ts
+++ src/todo/controller/todo.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from "@nestjs/common";
+import {Body, Controller, Delete, Param, Post} from "@nestjs/common";
+import {TodoService} from "../service/todo.service";
+import {TodoRequest} from "../service/dto/request/todo.dto.request";
+import {Todo} from "../entity/todo.entity";
 
-@Controller("api/v1/todo")
-export class TodoController { }
+@Controller("todo")
+export class TodoController {
+    constructor(
+        private todoService: TodoService) {
+    }
+
+    @Post("/:memberId/create")
+    async createTodo(@Param("memberId") memberId: number, @Body() todoRequest: TodoRequest): Promise<Todo> {
+        return await this.todoService.createTodo(todoRequest, memberId);
+    }
+
+    @Delete("/:memberId/delete/:todoId")
+    async deleteTodo(@Param("memberId") memberId: number, @Param("todoId") todoId: number): Promise<string> {
+        return await this.todoService.deleteTodo(todoId, memberId);
+    }
+}
