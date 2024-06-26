b57187ebedaea6e312b19c5bd68fec8b947fc041
IHFBEWANJEE <jeeeh@ihateflyingbugs.com>
2024-06-26 10:52:34 +0900

    edit / 정렬

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,25 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmConfigService } from './config/typeorm.config.service';
-import { MemberModule } from './member/module/member.module';
-import { TodoModule } from './todo/module/todo.module';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {ConfigModule} from '@nestjs/config';
+import {TypeOrmConfigService} from './config/typeorm.config.service';
+import {MemberModule} from './member/module/member.module';
+import {TodoModule} from './todo/module/todo.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true
-      }),
-    TypeOrmModule.forRootAsync({
-      useClass: TypeOrmConfigService
-    }),
-    MemberModule,
-    TodoModule
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true
+        }),
+        TypeOrmModule.forRootAsync({
+            useClass: TypeOrmConfigService
+        }),
+        MemberModule,
+        TodoModule
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
