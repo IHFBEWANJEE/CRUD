1f21539d672a1b2c2ba7f83efd1c6f2a01425d6b
IHFBEWANJEE <jeeeh@ihateflyingbugs.com>
2024-06-26 09:34:31 +0900

    edit / 정렬 및 오타 수정

--- src/member/service/member.service.ts
+++ src/member/service/member.service.ts
@@ -1,39 +1,40 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Member } from "../entity/member.entity";
-import { Repository } from "typeorm";
-import { MemberRequest } from "./dto/request/member.dto.request";
+import {Injectable} from "@nestjs/common";
+import {InjectRepository} from "@nestjs/typeorm";
+import {Member} from "../entity/member.entity";
+import {Repository} from "typeorm";
+import {MemberRequest} from "./dto/request/member.dto.request";
 
 @Injectable()
 export class MemberService {
     constructor(
         @InjectRepository(Member)
-        private memberRepository: Repository<Member>) { }
+        private memberRepository: Repository<Member>) {
+    }
 
-    async registerMember(memberRequest: MemberRequest): Promise<Member>{
+    async registerMember(memberRequest: MemberRequest): Promise<Member> {
         const member = new Member(memberRequest);
         await this.memberRepository.save(member)
         return member
     }
 
-    async deleteById(id: number): Promise<string>{
+    async deleteById(id: number): Promise<string> {
         const result = await this.memberRepository.delete(id)
-        return result ? "succesfully deleted" : "Something was wrong"
+        return result ? "successfully deleted" : "Something was wrong"
     }
 
-    async getAllMembers(): Promise<Member[]>{
+    async getAllMembers(): Promise<Member[]> {
         return await this.memberRepository.find()
     }
 
-    async findById(id: number):Promise<Member>{
-        return await this.memberRepository.findOneBy({ id: id })
+    async findById(id: number): Promise<Member> {
+        return await this.memberRepository.findOneBy({id: id})
     }
-    
-    async updateMemberName(id: number, newName: string): Promise<Member>{
+
+    async updateMemberName(id: number, newName: string): Promise<Member> {
         const result = await this.memberRepository.update(
-            {id:id},
+            {id: id},
             {name: newName}
         )
-        return await this.memberRepository.findOneBy({ id: id })
+        return await this.memberRepository.findOneBy({id: id})
     }
- }
+}
