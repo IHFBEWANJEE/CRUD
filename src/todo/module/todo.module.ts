aa7aa7e475fa9fe41ac5cb2a034e0c1e6ada96cf
IHFBEWANJEE <jeeeh@ihateflyingbugs.com>
2024-06-26 10:55:18 +0900

    add / MemberRepository 적용

--- src/todo/module/todo.module.ts
+++ src/todo/module/todo.module.ts
@@ -1,14 +1,16 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { Todo } from "../entity/todo.entity";
-import { TodoController } from "../controller/todo.controller";
-import { TodoService } from "../service/todo.service";
+import {Module} from "@nestjs/common";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {Todo} from "../entity/todo.entity";
+import {TodoController} from "../controller/todo.controller";
+import {TodoService} from "../service/todo.service";
+import {Member} from "../../member/entity/member.entity";
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([Todo])
+        TypeOrmModule.forFeature([Todo, Member]),
     ],
     controllers: [TodoController],
     providers: [TodoService]
 })
-export class TodoModule { }
+export class TodoModule {
+}
