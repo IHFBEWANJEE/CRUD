4e4ffa192f471773b1ba86d3ce341aaaf8a6cb4e
IHFBEWANJEE <jeeeh@ihateflyingbugs.com>
2024-06-27 10:45:18 +0900

    apply / MemberResolver 적용

--- src/member/module/member.module.ts
+++ src/member/module/member.module.ts
@@ -4,13 +4,14 @@ import {Member} from "../entity/member.entity";
 import {MemberController} from "../controller/member.controller";
 import {MemberService} from "../service/member.service";
 import {Todo} from "../../todo/entity/todo.entity";
+import {MemberResolver} from "../resolver/member.resolver";
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([Member, Todo])
     ],
     controllers: [MemberController],
-    providers: [MemberService]
+    providers: [MemberService, MemberResolver]
 })
 export class MemberModule {
 }
