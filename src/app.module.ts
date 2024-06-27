cbc7724f64e3261a36fa9fd35e8f9b0a2e88d8ad
IHFBEWANJEE <jeeeh@ihateflyingbugs.com>
2024-06-27 10:46:39 +0900

    add / graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,9 @@ import {ConfigModule} from '@nestjs/config';
 import {TypeOrmConfigService} from './config/typeorm.config.service';
 import {MemberModule} from './member/module/member.module';
 import {TodoModule} from './todo/module/todo.module';
+import {GraphQLModule} from "@nestjs/graphql";
+import {ApolloDriver, ApolloDriverConfig} from "@nestjs/apollo";
+import {ProductModule} from "./product/module/product.module";
 
 @Module({
     imports: [
@@ -15,8 +18,20 @@ import {TodoModule} from './todo/module/todo.module';
         TypeOrmModule.forRootAsync({
             useClass: TypeOrmConfigService
         }),
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver: ApolloDriver,
+            playground: true,
+            autoSchemaFile: true,
+            // typePaths: ['./**/*.graphql'], // 스키마 우선 접근 방식
+            // definitions: {
+            //     path: `${process.cwd()}/src/graphql.ts`, // GraphQL SDL 유형에 해당하는 Typescript 정의
+            //     //outputAs: 'class' 기본적으로 생성된 모든 Typescript 유형은 인터페이스. 대신 클래스로 생성하려면 이와 같이
+            // },
+            // plugins: [ApolloServerPluginLandingPageLocalDefault()] // Apollo SandBox
+        }),
         MemberModule,
-        TodoModule
+        TodoModule,
+        ProductModule
     ],
     controllers: [AppController],
     providers: [AppService],
