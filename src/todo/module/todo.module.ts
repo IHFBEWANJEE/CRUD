c79a91523286e3e534145d5248046f1e187540d8
IHFBEWANJEE <jeeeh@ihateflyingbugs.com>
2024-06-27 10:43:14 +0900

    add / Todo 리졸버

--- src/todo/module/todo.module.ts
+++ src/todo/module/todo.module.ts
@@ -4,13 +4,14 @@ import {Todo} from "../entity/todo.entity";
 import {TodoController} from "../controller/todo.controller";
 import {TodoService} from "../service/todo.service";
 import {Member} from "../../member/entity/member.entity";
+import {TodoResolver} from "../resolver/todo.resolver";
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([Todo, Member]),
     ],
     controllers: [TodoController],
-    providers: [TodoService]
+    providers: [TodoService, TodoResolver]
 })
 export class TodoModule {
 }
