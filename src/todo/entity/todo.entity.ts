de8a5e0b7a5fd4a166a8df1802f6b85546598901
IHFBEWANJEE <jeeeh@ihateflyingbugs.com>
2024-06-27 10:43:29 +0900

    apply / graphql 적용

--- src/todo/entity/todo.entity.ts
+++ src/todo/entity/todo.entity.ts
@@ -9,7 +9,9 @@ import {
 } from "typeorm";
 import {Member} from "../../member/entity/member.entity";
 import {TodoRequest} from "../service/dto/request/todo.dto.request";
+import {Field, ID, ObjectType} from "@nestjs/graphql";
 
+@ObjectType()
 @Entity("todo")
 export class Todo {
     constructor(partial?: Partial<TodoRequest>) {
@@ -19,21 +21,27 @@ export class Todo {
         }
     }
 
+    @Field(() => ID)
     @PrimaryGeneratedColumn()
     todoId: number
 
+    @Field(() => String)
     @Column()
     title: string
 
+    @Field(() => String)
     @Column()
     contents: string
 
+    @Field(() => Date)
     @CreateDateColumn()
     createdAt: Date
 
+    @Field(() => Date)
     @UpdateDateColumn()
     updatedAt: Date
 
+    @Field(() => Member, {nullable: false})
     @ManyToOne(() => Member, (member) => member.todos, {onDelete: 'CASCADE'})
     @JoinColumn({name: "memberId"})
     member: Member
