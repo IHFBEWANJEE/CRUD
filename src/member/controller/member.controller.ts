26f46f412431b0ed4f28b866f8c207e4aec70bc6
IHFBEWANJEE <jeeeh@ihateflyingbugs.com>
2024-06-27 10:46:22 +0900

    edit / 엔드포인트

--- src/member/controller/member.controller.ts
+++ src/member/controller/member.controller.ts
@@ -1,29 +1,30 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
-import { MemberService } from "../service/member.service";
-import { MemberRequest } from "../service/dto/request/member.dto.request";
-import { Member } from "../entity/member.entity";
+import {Body, Controller, Delete, Get, Param, Post, Put} from "@nestjs/common";
+import {MemberService} from "../service/member.service";
+import {MemberRequest} from "../service/dto/request/member.dto.request";
+import {Member} from "../entity/member.entity";
 
-@Controller("member")
+@Controller("members")
 export class MemberController {
-    constructor(private memberService: MemberService) { }
+    constructor(private memberService: MemberService) {
+    }
 
-    @Get("all")
+    @Get("")
     async findAllMembers(): Promise<Member[]> {
         return await this.memberService.getAllMembers();
     }
 
-    @Post("register")
-    async registerMember(@Body() memberRequest: MemberRequest): Promise<Member>{
+    @Post("")
+    async registerMember(@Body() memberRequest: MemberRequest): Promise<Member> {
         return await this.memberService.registerMember(memberRequest)
     }
 
-    @Put("update/:id")
+    @Put(":id")
     async updateMember(@Param("id") id: number, @Body('name') newName: string): Promise<Member | string> {
         return await this.memberService.updateMemberName(id, newName)
     }
 
-    @Delete("delete/:id")
-    async deleteMember(@Param("id") id: number): Promise<string>{
+    @Delete(":id")
+    async deleteMember(@Param("id") id: number): Promise<string> {
         return await this.memberService.deleteById(id)
     }
 }
